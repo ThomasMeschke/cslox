203f33b058233eb02476f1caef2aecf3b847bc5a
Thomas Meschke <thomas.meschke@logmein.com>
2020-10-03 16:10:11 +0200

    4.4-the-scanner-class

--- cslox/Scanner.cs
+++ cslox/Scanner.cs
@@ -6,6 +6,11 @@ namespace cslox
     internal class Scanner
     {
         private readonly string source;
+        private readonly List<Token> tokens = new List<Token>();
+
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
 
         public Scanner(string source)
         {
@@ -14,7 +19,19 @@ namespace cslox
 
         public List<Token> ScanTokens()
         {
-            throw new NotImplementedException();
+            while (!IsAtEnd())
+            {
+                start = current;
+                ScanToken();
+            }
+
+            tokens.Add(new Token(TokenType.EOF, string.Empty, null, line));
+            return tokens;
+        }
+
+        private bool IsAtEnd()
+        {
+            return current >= source.Length;
         }
     }
 }
