d10ca0ba86ebd08eb7d0b51da99cbd90f40c2ccc
Thomas Meschke <thomas.meschke@logmein.com>
2020-10-03 16:05:17 +0200

    4.2-lexemes-and-tokens

--- cslox/Token.cs
+++ cslox/Token.cs
@@ -2,5 +2,22 @@
 {
     internal class Token
     {
+        public TokenType Type { get; private set; }
+        public string Lexeme { get; private set; }
+        public object Literal { get; private set; }
+        public int Line { get; private set; }
+
+        public Token(TokenType type, string lexeme, object literal, int line)
+        {
+            Type = type;
+            Lexeme = lexeme;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} {Lexeme} {Literal}";
+        }
     }
 }
---
+++ cslox/TokenType.cs
@@ -0,0 +1,53 @@
+﻿namespace cslox
+{
+    internal enum TokenType
+    {
+        // single character tokens
+        LeftParen,
+        RightParen,
+        LeftBrace,
+        RightBrace,
+        Comma,
+        Dot,
+        Minus,
+        Plus,
+        Semicolon,
+        Slash,
+        Star,
+
+        // one or two character tokens
+        Bang,
+        BangEqual,
+        Equal,
+        EqualEqual,
+        Greater,
+        GreaterEqual,
+        Less,
+        LessEqual,
+
+        // literals
+        Identifier,
+        String,
+        Number,
+
+        //Keywords
+        And,
+        Or,
+        Var,
+        Fun,
+        Class,
+        If,
+        Else,
+        True,
+        False,
+        For,
+        While,
+        Return,
+        Nil,
+        Print,
+        Super,
+        This,
+
+        EOF
+    }
+}
