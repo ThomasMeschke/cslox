2cb458c89e2ec84f2dd2adedb81d5f1dadf1c058
Thomas Meschke <thomas.meschke@googlemail.com>
2022-04-10 13:31:55 +0200

    Add AST generator project and auto-generated Expression classes

---
+++ ast-gen/ASTGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ast_gen
+{
+    class ASTGenerator
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: ast-gen <output directory>");
+                Environment.Exit(64);
+            }
+            string outputDir = args[0];
+            DefineAST(outputDir, "Expression", new List<string>(){
+                "Binary     : Expression left, Token token, Expression right",
+                "Grouping   : Expression Expressionession",
+                "Literal    : object value",
+                "Unary      : Token token, Expression right"
+            });
+        }
+
+        private static void DefineAST(string outputDir, string baseName, List<string> types)
+        {
+            string path = Path.Combine(outputDir, baseName + ".cs");
+            StreamWriter writer = new StreamWriter(File.Open(path, FileMode.Create), System.Text.Encoding.UTF8);
+
+            writer.WriteLine("namespace cslox");
+            writer.WriteLine("{");
+            writer.WriteLine("    public abstract class " + baseName);
+            writer.WriteLine("    {");
+
+            foreach(string type in types)
+            {
+                string className = type.Split(":")[0].Trim();
+                string fieldList = type.Split(":")[1].Trim();
+                DefineType(writer, baseName, className, fieldList);
+            }
+
+            writer.WriteLine("    }");
+            writer.WriteLine("}");
+
+            writer.Close();
+        }
+
+        private static void DefineType(StreamWriter writer, string baseName, string className, string fieldList)
+        {
+            writer.WriteLine("        public class " + className + " : " + baseName);
+            writer.WriteLine("        {");
+
+            // Constructor.
+            writer.WriteLine("            public " + className + "(" + fieldList + ")");
+            writer.WriteLine("            {");
+
+            // Store parameters in fields.
+            string[] fields = fieldList.Split(new string[]{", "}, StringSplitOptions.None);
+            foreach(string field in fields)
+            {
+                string name = field.Split(" ")[1];
+                writer.WriteLine("                this." + name.ToUpperFirst() + " = " + name + ";");
+            }
+            writer.WriteLine("            }");
+
+            // Fields. 
+            writer.WriteLine();
+            foreach(string field in fields)
+            {
+                string type = field.Split(" ")[0];
+                string name = field.Split(" ")[1];
+                writer.WriteLine("        public readonly " + type + " " + name.ToUpperFirst() + ";");
+            }
+
+            writer.WriteLine("        }");
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static string ToUpperFirst(this string value)
+        {
+            return value[0].ToString().ToUpper() + value.Substring(1);
+        }
+    }
+}
---
+++ cslox/Expression.cs
@@ -0,0 +1,48 @@
+﻿namespace cslox
+{
+    public abstract class Expression
+    {
+        public class Binary : Expression
+        {
+            public Binary(Expression left, Token token, Expression right)
+            {
+                this.Left = left;
+                this.Token = token;
+                this.Right = right;
+            }
+
+        public readonly Expression Left;
+        public readonly Token Token;
+        public readonly Expression Right;
+        }
+        public class Grouping : Expression
+        {
+            public Grouping(Expression Expressionession)
+            {
+                this.Expressionession = Expressionession;
+            }
+
+        public readonly Expression Expressionession;
+        }
+        public class Literal : Expression
+        {
+            public Literal(object value)
+            {
+                this.Value = value;
+            }
+
+        public readonly object Value;
+        }
+        public class Unary : Expression
+        {
+            public Unary(Token token, Expression right)
+            {
+                this.Token = token;
+                this.Right = right;
+            }
+
+        public readonly Token Token;
+        public readonly Expression Right;
+        }
+    }
+}
